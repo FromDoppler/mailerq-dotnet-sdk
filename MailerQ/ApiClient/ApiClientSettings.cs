9f7fbc47b4999efe5f6cfd31969394e958f84b4a
bamaro <amaro.belen.a@gmail.com>
2020-03-27 10:23:09 -0300

    Add Api Client settings

---
+++ MailerQ/ApiClient/ApiClientSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MailerQ.ApiClient
+{
+    public class ApiClientSettings
+    {
+        public string Url { get; set; }
+        public string Token { get; set; }
+        public string Version { get; set; }
+    }
+}
