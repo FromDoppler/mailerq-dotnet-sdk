b4c18839d9c73834a913ee598d5bfddf34f68cab
bamaro <amaro.belen.a@gmail.com>
2020-03-27 10:23:50 -0300

    Add Api Client interface

---
+++ MailerQ/ApiClient/IApiClient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MailerQ.ApiClient
+{
+    public interface IApiClient
+    {
+        bool Post(Error error);
+        bool Post(Pause pause);
+        bool Post(Inject inject);
+        bool Delete(Error error);
+        bool Delete(Pause pause);
+        List<Error> Get(Error error);
+        List<Pause> Get(Pause pause);
+    }
+}
