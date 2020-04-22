63c6ee760a2965bddc10a2f7bd441e5f4e08a5e8
bamaro <amaro.belen.a@gmail.com>
2020-04-22 11:59:07 -0300

    Add MailerQ Rest API types

---
+++ MailerQ/RestApi/Pause.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+
+namespace MailerQ.RestApi
+{
+    /// <summary>
+    /// Using this endpoint MailerQ allows you to pause almost any combination of pool/mta and domain/ip.
+    /// </summary>
+    [JsonObject(
+       NamingStrategyType = typeof(LowercaseNamingStrategy),
+       ItemNullValueHandling = NullValueHandling.Ignore
+    )]
+    public class Pause
+    {
+        /// <summary>
+        /// Pool that the pause applies to.
+        /// </summary>
+        public string Pool { get; set; }
+
+        /// <summary>
+        /// MTA IP that the pause applies to.
+        /// </summary>
+        public string Mta { get; set; }
+
+        /// <summary>
+        /// Domain that the pause applies to.
+        /// </summary>
+        public string Domain { get; set; }
+
+        /// <summary>
+        /// Remote IP that the pause applies to.
+        /// </summary>
+        public string Ip { get; set; }
+
+        /// <summary>
+        /// The tag that the pause applies to.
+        /// </summary>
+        public string Tag { get; set; }
+
+        /// <summary>
+        /// Whether or not this pause is for the entire cluster or only this instance.
+        /// </summary>
+        public bool Cluster { get; set; }
+    }
+}
---
+++ MailerQ/RestApiInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MailerQ
+{
+    /// <summary>
+    /// Message injected via MailerQ REST API hold information about who connected and the name of the authentication token in the http property.
+    /// </summary>
+    public class RestApiInfo
+    {
+        /// <summary>
+        /// Name of the authentication token
+        /// </summary>
+        public string Authentication { get; set; }
+
+        /// <summary>
+        /// Remote ip who connected
+        /// </summary>
+        public string RemoteIp { get; set; }
+
+        /// <summary>
+        /// Remote port who connected
+        /// </summary>
+        public string RemotePort { get; set; }
+    }
+}
