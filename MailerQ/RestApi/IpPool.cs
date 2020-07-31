7becd1b188c113523ce2832c6a27b7980a7387ab
Claudio Rodrigo Pereyra Diaz <cpereyra@makingsense.com>
2020-07-31 13:48:05 -0300

    feat: add types for IP Pools rest api endpoint
    See https://www.mailerq.com/documentation/5.8/rest-api-v1-pools

---
+++ MailerQ/RestApi/IpPool.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+
+namespace MailerQ.RestApi
+{
+    /// <summary>
+    /// Pool of IPs
+    /// MailerQ offers IP Pools for easy management of your sending IP addresses.
+    /// </summary>
+    [JsonObject(
+        NamingStrategyType = typeof(LowercaseNamingStrategy),
+        ItemNullValueHandling = NullValueHandling.Ignore
+    )]
+    public class IpPool
+    {
+        /// <summary>
+        /// The name of the pool
+        /// </summary>
+        [JsonProperty(Required = Required.Always)]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Description of the pool, for human use.
+        /// </summary>
+        public string Description { get; set; }
+    }
+
+    /// <summary>
+    /// IP Address
+    /// MailerQ offers IP Pools for easy management of your sending IP addresses.
+    /// </summary>
+    [JsonObject(
+        NamingStrategyType = typeof(LowercaseNamingStrategy),
+        ItemNullValueHandling = NullValueHandling.Ignore
+    )]
+    public class IpAddress
+    {
+        /// <summary>
+        /// The IP address
+        /// </summary>
+        [JsonProperty(Required = Required.Always)]
+        public string Ip { get; set; }
+        /// <summary>
+        /// The name of the pool
+        /// </summary>
+        [JsonProperty(Required = Required.Always)]
+        public string Name { get; set; }
+
+    }
+}
