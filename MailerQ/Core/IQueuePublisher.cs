104164c15b4aef8bf1a2efab74fcb75b670ff873
Claudio Rodrigo Pereyra Diaz <cpereyra@makingsense.com>
2020-09-17 09:07:09 -0300

    Revert "refactor: remove redundant methods"
    This reverts commit 7cc7fa140fefd14233e1c9bd470e9491e45786bc.

--- MailerQ/Core/IQueuePublisher.cs
+++ MailerQ/Core/IQueuePublisher.cs
@@ -7,7 +7,9 @@ namespace MailerQ
 {
     public interface IQueuePublisher : IDisposable
     {
+        void Publish(OutgoingMessage message);
         void Publish(OutgoingMessage outgoingMessage, string queueName = QueueName.Outbox);
+        Task PublishAsync(OutgoingMessage message);
         Task PublishAsync(OutgoingMessage outgoingMessage, string queueName = QueueName.Outbox);
         void Publish(IEnumerable<OutgoingMessage> messages, string queueName = QueueName.Outbox);
         Task PublishAsync(IEnumerable<OutgoingMessage> messages, string queueName = QueueName.Outbox);
