62296f274ca5a326357a5189a17634f2de79f09e
Claudio Rodrigo Pereyra Diaz <cpereyra@makingsense.com>
2020-11-06 13:24:11 -0300

    feat: define interfaz for delcare queues

---
+++ MailerQ/Core/IQueueDeclarer.cs
@@ -0,0 +1,29 @@
+﻿using MailerQ.Conventions;
+
+namespace MailerQ
+{
+    /// <summary>
+    /// Interfaz to declare queues
+    /// </summary>
+    public interface IQueueDeclarer
+    {
+        /// <summary>
+        /// Declare a deadletter queue for publish OutgoingMessages
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="addQueueNamePrefix">Add prefix to queue name</param>
+        /// <param name="durable"></param>
+        /// <param name="messageTtl"></param>
+        /// <param name="maxPriority"></param>
+        /// <param name="deadLetterExchange"></param>
+        /// <param name="deadLetterRoutingKey"></param>
+        void DeclareDeadLetterQueueForPublish(
+            string name,
+            bool addQueueNamePrefix = true,
+            bool durable = true,
+            int messageTtl = 10,
+            int maxPriority = (int)Priority.High,
+            string deadLetterExchange = "",
+            string deadLetterRoutingKey = QueueName.Outbox);
+    }
+}
