c38d527fa1727f08119f4a97347b8af90b6c52fd
Claudio Rodrigo Pereyra Diaz <cpereyra@makingsense.com>
2021-06-25 10:18:02 -0300

    feat: define interfaces for interact with MailerQ Rest Api

---
+++ MailerQ/RestApi/IRestApiRequest.cs
@@ -0,0 +1,14 @@
+﻿namespace MailerQ.RestApi
+{
+    /// <summary>
+    /// Contract that represent a basic MailerQ Rest API request
+    /// </summary>
+    /// <typeparam name="T">Model type that accept the endpoint</typeparam>
+    public interface IRestApiRequest<T> where T : IRestApiModel
+    {
+        /// <summary>
+        /// The query string parameters
+        /// </summary>
+        string QueryString { get; }
+    }
+}
