3b55035cbe2350d874c038d6a23b7e24a4945723
Claudio Rodrigo Pereyra Diaz <cpereyra@makingsense.com>
2022-05-13 11:04:45 -0300

    refactor: add MailerQ json serializer datetime converter

---
+++ MailerQ/Json/MailerQDateTimeConverter.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json.Converters;
+
+namespace MailerQ.Json
+{
+    /// <summary>
+    /// Converter to serialize date time with format used by MailerQ
+    /// </summary>
+    public class MailerQDateTimeConverter : IsoDateTimeConverter
+    {
+        /// <summary>
+        /// Constructor of converter for MailerQ date time format
+        /// </summary>
+        public MailerQDateTimeConverter()
+        {
+            DateTimeFormat = "yyyy-MM-dd HH:mm:ss";
+        }
+    }
+}
